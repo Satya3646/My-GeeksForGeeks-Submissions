4ce9da0cddee21c21bbe83b263f8f6042eb58027
Satyanarayana Chittidi <153839365+Satya3646@users.noreply.github.com>
2025-12-03 21:10:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
+++ Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
@@ -15,19 +15,19 @@ class Solution {
   public:
     vector<int> preOrder(Node* root) {
         // code here
-        stack <Node*> stk;
+        stack <Node*> stk; // Stack to mimic call stack
         vector <int> ans;
-        if(!root)
+        if(!root) // if tree is empty return empty answer
             return ans;
-        stk.push(root);
+        stk.push(root); // Put root in stack to start traversal
         while(!stk.empty())
         {
-            Node *node = stk.top();
+            Node *node = stk.top(); // Extract the top node
             stk.pop();
-            ans.push_back(node->data);
-            if(node->right)
+            ans.push_back(node->data); // Put the value into the answer
+            if(node->right) // Put the right node into stack since it needs to be evaluater later than the left node
                 stk.push(node->right);
-            if(node->left)
+            if(node->left) // Put the left node on top of the right node since it need to be evaluated first
                 stk.push(node->left);
         }
         return ans;
